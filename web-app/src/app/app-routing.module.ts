59a8bc8c5897ece8793ad72c0097683604a8f3eb
pedrocostab <pedrocostab@gmail.com>
2023-03-27 16:50:45 -0300

    criacao da rota de login e lista de usuarios

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
+import { LoginComponent } from './login/login.component';
 import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 
 const routes: Routes = [
   {path: '', component:IndexComponent},
   {path: 'register', component:RegistroUsuarioComponent},
- // {path: 'login', component:},
-  {path: '', component:IndexComponent}
+  {path: 'login', component:LoginComponent},
+  {path: 'user', component:ListaUsuarioComponent}
 ];
 
 @NgModule({
