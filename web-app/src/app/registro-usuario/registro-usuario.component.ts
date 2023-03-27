0ce9d1cd1fc99c5b0c735d5e2d02ab9be7fce150
pedrocostab <pedrocostab@gmail.com>
2023-03-27 17:41:25 -0300

    adicao de estrutura base do cadastro de usuario

--- web-app/src/app/registro-usuario/registro-usuario.component.ts
+++ web-app/src/app/registro-usuario/registro-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-registro-usuario',
@@ -7,8 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private builder: FormBuilder, private service: AuthService, private router: Router) {
 
+  }
+
+
+  registerform=this.builder.group({
+    id:this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    name:this.builder.control('', Validators.required),
+    password:this.builder.control('', Validators.compose([Validators.required, Validators.minLength(6)])),
+    email:this.builder.control('', Validators.compose([Validators.required, Validators.email])),
+    gender:this.builder.control('male'),
+    role:this.builder.control(''),
+    isactive:this.builder.control(false)
+  })
+
+  proceedregister() {
+    if (this.registerform.valid) {
+ 
+    } else {
+      console.error('Por favor, insira uma senha com no mínimo 6 caracteres')
+    }
+  }
+  
   ngOnInit() {
   }
 
