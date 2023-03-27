d8a5307633484c47467c25116d5762f4f56ceee8
pedrocostab <pedrocostab@gmail.com>
2023-03-27 16:44:33 -0300

    adicao da rota de registro

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:IndexComponent},
+  {path: 'register', component:RegistroUsuarioComponent},
+ // {path: 'login', component:},
+  {path: '', component:IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
