383d20f6ba29841b6e0347b1d9d8e7303646e4d2
pedrocostab <pedrocostab@gmail.com>
2023-03-28 12:35:30 -0300

    aplicacao do autenticador para as telas home e user

--- Dizer/src/app/app-routing.module.ts
+++ Dizer/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserlistingComponent } from './userlisting/userlisting.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
+  {path:'', component:HomeComponent, canActivate:[AuthGuard]},
   {path:'register', component:RegisterComponent},
   {path:'login', component:LoginComponent},
-  {path:'user', component:UserlistingComponent}
+  {path:'user', component:UserlistingComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
