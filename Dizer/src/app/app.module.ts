9bcf44a60bd78269c053cd28356f624c44331052
pedrocostab <pedrocostab@gmail.com>
2023-03-28 10:22:26 -0300

    criacao de componentes do angular

--- Dizer/src/app/app.module.ts
+++ Dizer/src/app/app.module.ts
@@ -7,10 +7,20 @@ import { MaterialModule } from 'src/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { UserlistingComponent } from './userlisting/userlisting.component';
+import { UpdatepopupComponent } from './updatepopup/updatepopup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    UserlistingComponent,
+    UpdatepopupComponent
   ],
   imports: [
     BrowserModule,
