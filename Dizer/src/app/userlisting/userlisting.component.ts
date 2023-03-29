e640cb8bb41d18e41628305c18ff36f64a3e02fe
pedrocostab <pedrocostab@gmail.com>
2023-03-29 00:46:10 -0300

    adicao da lista de usuario

--- Dizer/src/app/userlisting/userlisting.component.ts
+++ Dizer/src/app/userlisting/userlisting.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-userlisting',
@@ -13,11 +14,19 @@ export class UserlistingComponent {
   }
 
   userlist: any;
+  dataSource:any;
 
   Loaduser() {
     this.service.GetAll().subscribe(res => {
       this.userlist = res;
+      this.dataSource = new MatTableDataSource(this.userlist)
     });
+  }
+
+  displayedColumns: string[] = ['username', 'name', 'email', 'role' , 'status', 'action'];
+
+  UpdateUser(code:any){
+
   }
 
 }
