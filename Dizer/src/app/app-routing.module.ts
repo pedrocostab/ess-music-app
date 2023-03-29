57257eb2859077fdbfcf8fde5a367713ba5e51f1
pedrocostab <pedrocostab@gmail.com>
2023-03-29 00:48:46 -0300

    adicao do componente da pagina inicial de usuario logado

--- Dizer/src/app/app-routing.module.ts
+++ Dizer/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserlistingComponent } from './userlisting/userlisting.component';
 import { ListaMusicasComponent } from './lista-musicas/lista-musicas.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
 // import { MusicasComponent } from './musicas/musicas.component';
 
 
@@ -16,7 +17,8 @@ const routes: Routes = [
   {path:'login', component:LoginComponent},
   {path:'user', component:UserlistingComponent, canActivate:[AuthGuard]},
   // {path: 'cadastrar-musicas', component: MusicasComponent},
-  {path: 'lista-musicas', component: ListaMusicasComponent, canActivate:[AuthGuard]}
+  {path: 'lista-musicas', component: ListaMusicasComponent, canActivate:[AuthGuard]},
+  {path: 'initial-page', component: InitialPageComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
