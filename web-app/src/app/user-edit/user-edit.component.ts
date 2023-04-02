4b8765c7e8471f2e1492c908cd14466bd8ff2fb6
pedrocostab <pedrocostab@gmail.com>
2023-04-02 19:18:44 -0300

    remodelagem do front para aparecer os dados de perfil dinamicamente

--- web-app/src/app/user-edit/user-edit.component.ts
+++ web-app/src/app/user-edit/user-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent {
+
+  constructor(private service: AuthService) {
+    this.Loadinfouser();
+  }
+
+  user: any;
+
+  Loadinfouser(){
+    this.user = this.service.Getbycode(sessionStorage.getItem('username')).subscribe(res=> this.user);
+  }
 
 }
