7fba6ce39d9ccb874e416d32f496a3e0b5c1323e
pedrocostab <pedrocostab@gmail.com>
2023-04-02 21:02:06 -0300

    Adicao da funcionalidade do usuario trocar sua senha

--- web-app/src/app/alterar-senha-popup/alterar-senha-popup.component.ts
+++ web-app/src/app/alterar-senha-popup/alterar-senha-popup.component.ts
@@ -14,4 +14,41 @@ export class AlterarSenhaPopupComponent {
     @Inject(MAT_DIALOG_DATA) public data: any, private toastr: ToastrService,
     private dialog: MatDialogRef<AlterarSenhaPopupComponent>) {
   }
+  user: any;
+  rolelist: any;
+
+
+
+  ngOnInit(): void {
+    this.user = this.service.Getbycode(sessionStorage.getItem('username')).subscribe(res=> {
+      this.user = res;
+      this.registerform.setValue({
+        id: this.user.id, name: this.user.name, email: this.user.email,
+        password: this.user.password, role: this.user.role, gender: this.user.gender,
+        isactive: this.user.isactive
+      })
+    })
+  }
+
+  registerform = this.builder.group({
+    id: this.builder.control(''),
+    name: this.builder.control(''),
+    password: this.builder.control(''),
+    email: this.builder.control(''),
+    gender: this.builder.control(''),
+    role: this.builder.control(''),
+    isactive: this.builder.control(false)
+  });
+
+  alterarSenha() {
+    if (this.registerform.valid) {
+      this.service.Updateuser(this.registerform.value.id, this.registerform.value).subscribe(res => {
+        this.toastr.success('Senha alterada com sucesso!');
+        this.dialog.close();
+      })
+    } else {
+      this.toastr.warning('A senha deve conter pelo menos 6 digitos!')
+    }
+  }
+
 }
