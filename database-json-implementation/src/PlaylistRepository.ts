dd3d734ff89d16b6f9d48b0048f6a39f36f4458a
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-06 20:06:42 -0300

    Implementado PlaylistRepository

--- database-json-implementation/src/PlaylistRepository.ts
+++ database-json-implementation/src/PlaylistRepository.ts
@@ -1,20 +1,68 @@
 import { IPlaylistRepository } from 'database-abstraction-layer';
-import { Playlist } from 'music-app-models';
+import { copy, Playlist } from 'music-app-models';
+import { JsonDB } from './json-handling/json-handling';
 
 export class PlaylistRepository implements IPlaylistRepository {
+    jsonDb: JsonDB;
+
+    constructor(jsonDb: JsonDB){
+        this.jsonDb = jsonDb;
+    }
+
     getAllByOwnerEmail(email: string): Playlist[] {
-        throw new Error('Method not implemented.');
+        return this.jsonDb.playlists.filter(p => p.owner_email == email).map((p) => copy(p));
     }
+
     add(instance: Playlist): boolean {
-        throw new Error('Method not implemented.');
+        if(
+            this.jsonDb.playlists.find(
+                p => 
+                    p.owner_email == instance.owner_email &&
+                    p.number == instance.number
+            )
+        )
+            return false;
+        
+        this.jsonDb.playlists.push(instance);
+        this.jsonDb.saveChanges();
+
+        return true;
     }
+
     update(instance: Playlist): boolean {
-        throw new Error('Method not implemented.');
+        let usr = this.jsonDb.playlists.find(
+            p => 
+                p.owner_email == instance.owner_email &&
+                p.number == instance.number
+        );
+
+        if(!usr)
+            return false;
+        
+        Object.assign(usr, instance);
+        this.jsonDb.saveChanges();
+
+        return true;
     }
+
     delete(instance: Playlist): boolean {
-        throw new Error('Method not implemented.');
+        let index = this.jsonDb.playlists.indexOf(instance);
+
+        if(index == -1)
+            return false;
+        
+        this.jsonDb.playlists = this.jsonDb.playlists.filter(
+            p => p.owner_email != instance.owner_email && p.number != instance.number
+        );
+
+        this.jsonDb.saveChanges();
+
+        return true;
     }
+
     getAll(): Playlist[] {
-        throw new Error('Method not implemented.');
+        return this.jsonDb.playlists.map(
+            p => copy(p)
+        );
     }
 }
