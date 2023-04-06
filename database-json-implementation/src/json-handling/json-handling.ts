b02c3b1953d5fd28ff39c73f9dc313c7a27484c3
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-06 19:44:40 -0300

    Implementado handling de json no database

---
+++ database-json-implementation/src/json-handling/json-handling.ts
@@ -0,0 +1,47 @@
+import fs = require('fs');
+import { Category, Playlist, PlaylistCategory, PlaylistSong, Song, User, UserFollowPlaylist, UserSongHistory } from 'music-app-models';
+
+
+class JsonDB {
+    users: User[];
+    songs: Song[];
+    playlists: Playlist[];
+    categories: Category[];
+    playlistCategories: PlaylistCategory[];
+    playlistSongs: PlaylistSong[];
+    userFollowPlaylists: UserFollowPlaylist[];
+    userSongHistories: UserSongHistory[];
+
+    dbFilePath:string;
+
+    writing: boolean = false;
+
+    saveChanges(){
+        if(this.writing)
+            return;
+        
+        this.writing = true;
+
+        fs.writeFile(this.dbFilePath, JSON.stringify(this), null, () => {this.writing = false});
+    }
+}
+
+function initDatabase(dbFilePath: string):JsonDB{
+    let jsonDatabase:JsonDB;
+    
+    fs.readFile(dbFilePath, function (err: any, data: any) {
+        if (err) {
+            console.error(err)
+        }
+    
+        jsonDatabase = JSON.parse(data);
+    })
+
+    return jsonDatabase;
+}
+
+
+export {
+    JsonDB,
+    initDatabase
+}
