24df6fb7a3bb34ab1b84f47984a6d52370453da0
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-06 16:55:38 -0300

    Adicionado getAll na interface base IRepository

--- database-abstraction-layer/src/IRepository.ts
+++ database-abstraction-layer/src/IRepository.ts
@@ -2,4 +2,5 @@ export interface IRepository<Entity>{
     add(instance: Entity): boolean;
     update(instance: Entity): boolean;
     delete(instance: Entity): boolean;
+    getAll(): Entity[];
 };
