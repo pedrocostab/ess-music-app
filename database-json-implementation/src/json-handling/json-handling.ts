f5095d997bfa30040ddb6cc588e5dc8306092256
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-06 22:32:30 -0300

    Corrigidos projetos de database

--- database-json-implementation/src/json-handling/json-handling.ts
+++ database-json-implementation/src/json-handling/json-handling.ts
@@ -3,14 +3,14 @@ import { Category, Playlist, PlaylistCategory, PlaylistSong, Song, User, UserFol
 
 
 class JsonDB {
-    users: User[];
-    songs: Song[];
-    playlists: Playlist[];
-    categories: Category[];
-    playlistCategories: PlaylistCategory[];
-    playlistSongs: PlaylistSong[];
-    userFollowPlaylists: UserFollowPlaylist[];
-    userSongHistories: UserSongHistory[];
+    users: User[] = [];
+    songs: Song[] = [];
+    playlists: Playlist[] = [];
+    categories: Category[] = [];
+    playlistCategories: PlaylistCategory[] = [];
+    playlistSongs: PlaylistSong[] = [];
+    userFollowPlaylists: UserFollowPlaylist[] = [];
+    userSongHistories: UserSongHistory[] = [];
 
     dbFilePath:string;
 
@@ -28,7 +28,15 @@ class JsonDB {
 
 function initDatabase(dbFilePath: string):JsonDB{
     let jsonDatabase:JsonDB;
-    
+
+    if(!fs.existsSync(dbFilePath)){
+        jsonDatabase = new JsonDB();
+
+        fs.writeFileSync(dbFilePath, JSON.stringify(jsonDatabase));
+
+        return jsonDatabase;
+    }
+
     fs.readFile(dbFilePath, function (err: any, data: any) {
         if (err) {
             console.error(err)
