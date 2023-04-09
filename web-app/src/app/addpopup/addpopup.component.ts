2d2ccbd7a510cb9b3db57307019de0db3ebf5eeb
pedrocostab <pedrocostab@gmail.com>
2023-04-09 18:36:39 -0300

    ajuste no adicionar novo usuario pelo admin

--- web-app/src/app/addpopup/addpopup.component.ts
+++ web-app/src/app/addpopup/addpopup.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import * as bcrypt from 'bcryptjs';
+
 
 @Component({
   selector: 'app-addpopup',
@@ -45,6 +47,7 @@ export class AddpopupComponent {
 
   adduser() {
     if (this.registerform.valid) {
+      this.registerform.value.password = await bcrypt.hash(this.registerform.value.password ?? '', 10);
       this.service.Adduser(this.registerform.value).subscribe(res => {
         this.toastr.success('Usuário cadastrado com sucesso!');
         this.dialog.close();
