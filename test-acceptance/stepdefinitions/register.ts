556baa544a55a8a01bf5768c1223d7bcbd12751b
pedrocostab <pedrocostab@gmail.com>
2023-04-09 10:39:30 -0300

    Ajuste na feature de playlist

--- test-acceptance/stepdefinitions/register.ts
+++ test-acceptance/stepdefinitions/register.ts
@@ -1,97 +1,97 @@
-import { defineSupportCode } from 'cucumber';
-import { browser, $, element, ElementArrayFinder, by } from 'protractor';
-let chai = require('chai').use(require('chai-as-promised'));
-let expect = chai.expect;
-import request = require("request-promise");
+// import { defineSupportCode } from 'cucumber';
+// import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+// let chai = require('chai').use(require('chai-as-promised'));
+// let expect = chai.expect;
+// import request = require("request-promise");
 
-var base_url = "http://localhost:3000/user";
+// var base_url = "http://localhost:3000/user";
 
-let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
-let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+// let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
+// let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
 
-let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+// let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 
-async function criarUsuario(id, name, password, email) {
-    await $("input[name='id']").sendKeys(<string> id);
-    await $("input[name='name']").sendKeys(<string> name);
-    await $("input[name='password']").sendKeys(<string> password);
-    await $("input[name='email']").sendKeys(<string> email);    
-    await element(by.buttonText('Enviar')).click();
-}
+// async function criarUsuario(id, name, password, email) {
+//     await $("input[name='id']").sendKeys(<string> id);
+//     await $("input[name='name']").sendKeys(<string> name);
+//     await $("input[name='password']").sendKeys(<string> password);
+//     await $("input[name='email']").sendKeys(<string> email);    
+//     await element(by.buttonText('Enviar')).click();
+// }
 
-async function assertTamanhoEqual(set,n) {
-    await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
-}
+// async function assertTamanhoEqual(set,n) {
+//     await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
+// }
 
-async function assertElementsWithSameCPFAndName(n,cpf,name) { 
-    var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-    var samecpfsandname = allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name)));
-    await assertTamanhoEqual(samecpfsandname,n);
-}
+// async function assertElementsWithSameCPFAndName(n,cpf,name) { 
+//     var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+//     var samecpfsandname = allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name)));
+//     await assertTamanhoEqual(samecpfsandname,n);
+// }
 
-async function assertElementsWithSameCPF(n,cpf) {
-    var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-    var samecpfs = allalunos.filter(elem => sameCPF(elem,cpf));
-    await assertTamanhoEqual(samecpfs,n); 
-}
+// async function assertElementsWithSameCPF(n,cpf) {
+//     var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+//     var samecpfs = allalunos.filter(elem => sameCPF(elem,cpf));
+//     await assertTamanhoEqual(samecpfs,n); 
+// }
 
-defineSupportCode(function ({ Given, When, Then }) {
+// defineSupportCode(function ({ Given, When, Then }) {
 
 
 
-    Given(/^I am on the "([^\"]*)" page$/, async (page) => {
-        await browser.get("http://localhost:4200/"+page);
-    })
+//     Given(/^I am on the "([^\"]*)" page$/, async (page) => {
+//         await browser.get("http://localhost:4200/"+page);
+//     })
 
-    When(/^I click on "([^\"]*)"$/, async (buttonText) => {
-        await $(buttonText).click();
-    });
+//     When(/^I click on "([^\"]*)"$/, async (buttonText) => {
+//         await $(buttonText).click();
+//     });
 
-    // Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
-    //     await assertElementsWithSameCPF(0,cpf);
-    // });
+//     // Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
+//     //     await assertElementsWithSameCPF(0,cpf);
+//     // });
 
-    // When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-    //     await criarAluno(name,cpf);
-    // });
+//     // When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+//     //     await criarAluno(name,cpf);
+//     // });
 
-    // Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
-    //     await assertElementsWithSameCPFAndName(1,cpf,name);
-    // });
+//     // Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+//     //     await assertElementsWithSameCPFAndName(1,cpf,name);
+//     // });
 
-    // Given(/^I can see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
-    //     await criarAluno("Clarissa",cpf);
-    //     await assertElementsWithSameCPF(1,cpf); 
-    // });
+//     // Given(/^I can see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
+//     //     await criarAluno("Clarissa",cpf);
+//     //     await assertElementsWithSameCPF(1,cpf); 
+//     // });
 
-    // Then(/^I cannot see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
-    //     await assertElementsWithSameCPFAndName(0,cpf,name);
-    // });
+//     // Then(/^I cannot see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+//     //     await assertElementsWithSameCPFAndName(0,cpf,name);
+//     // });
 
-    // Then(/^I can see an error message$/, async () => {
-    //     var allmsgs : ElementArrayFinder = element.all(by.name('msgcpfexistente'));
-    //     await assertTamanhoEqual(allmsgs,1);
-    // });
+//     // Then(/^I can see an error message$/, async () => {
+//     //     var allmsgs : ElementArrayFinder = element.all(by.name('msgcpfexistente'));
+//     //     await assertTamanhoEqual(allmsgs,1);
+//     // });
 
-    // Given(/^the system has no student with CPF "(\d*)"$/, async (cpf) => {
-    //    await request.get(base_url + "alunos")
-    //             .then(body => 
-    //                expect(body.includes(`"cpf":"${cpf}"`)).to.equal(false));
-    // });
+//     // Given(/^the system has no student with CPF "(\d*)"$/, async (cpf) => {
+//     //    await request.get(base_url + "alunos")
+//     //             .then(body => 
+//     //                expect(body.includes(`"cpf":"${cpf}"`)).to.equal(false));
+//     // });
 
-    // When(/^I register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-    //     let aluno = {"nome": name, "cpf" : cpf, "email":""};
-    //     var options:any = {method: 'POST', uri: (base_url + "aluno"), body:aluno, json: true};
-    //     await request(options)
-    //           .then(body => 
-    //                expect(JSON.stringify(body)).to.equal(
-    //                    '{"success":"O aluno foi cadastrado com sucesso"}'));
-    // });
+//     // When(/^I register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+//     //     let aluno = {"nome": name, "cpf" : cpf, "email":""};
+//     //     var options:any = {method: 'POST', uri: (base_url + "aluno"), body:aluno, json: true};
+//     //     await request(options)
+//     //           .then(body => 
+//     //                expect(JSON.stringify(body)).to.equal(
+//     //                    '{"success":"O aluno foi cadastrado com sucesso"}'));
+//     // });
 
-    // Then(/^the system now stores "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-    //     let resposta = `{"nome":"${name}","cpf":"${cpf}","email":"","metas":{}`;
-    //     await request.get(base_url + "alunos")
-    //                  .then(body => expect(body.includes(resposta)).to.equal(true));
-    // });
+//     // Then(/^the system now stores "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+//     //     let resposta = `{"nome":"${name}","cpf":"${cpf}","email":"","metas":{}`;
+//     //     await request.get(base_url + "alunos")
+//     //                  .then(body => expect(body.includes(resposta)).to.equal(true));
+//     // });
 
-})
+// })
