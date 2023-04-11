cc2dd10f70b805e0e2915637426f2aae39145e47
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-11 17:37:33 -0300

    Correções nos testes e implementações de cadastro e alteração de artistas

--- web-app/src/app/cadastra-artista/cadastra-artista.component.ts
+++ web-app/src/app/cadastra-artista/cadastra-artista.component.ts
@@ -3,6 +3,7 @@ import { CadastraArtistaService } from './cadastra-artista.service';
 import { Artista } from '../artista/artista';
 import { Categoria } from '../criar-categoria/categoria';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cadastra-artista',
@@ -10,16 +11,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./cadastra-artista.component.css']
 })
 export class CadastraArtistaComponent {
-  constructor(private cadastraArtistaService: CadastraArtistaService, private http: HttpClient) {}
+  constructor(
+    private cadastraArtistaService: CadastraArtistaService,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) {}
   artista: Artista = new Artista();
   artistas: Artista[] = [];
   categorias: Categoria[] = [];
   categoriasSelecionadas: Categoria[] = [];
 
   createArtista(c: Artista): void {
+    if(
+      c.nome == "" ||
+      c.url_foto_artista == ""
+    ){
+      this.toastr.error('Campo inválido!');
+      return;
+    }
+    
      this.cadastraArtistaService.create(c)
      .subscribe(result => {
            if (result) {
+              this.toastr.success('Artista cadastrado!');
               this.artistas.push(<Artista> result);
               this.artista = new Artista();
            }
