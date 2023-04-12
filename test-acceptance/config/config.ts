8bc105bf289efe8d375de35c861233a32a18b55a
pedrocostab <pedrocostab@gmail.com>
2023-04-11 23:31:29 -0300

    Todos os testes da feature de registro/edicao de usuario implementado

--- test-acceptance/config/config.ts
+++ test-acceptance/config/config.ts
@@ -17,7 +17,7 @@ export let config: Config = {
     frameworkPath: require.resolve('protractor-cucumber-framework'),
 
     specs: [
-        '../../features/teste.feature'
+        '../../features/*.feature'
     ],
 
     onPrepare: () => {
@@ -30,6 +30,6 @@ export let config: Config = {
         compiler: "ts:ts-node/register",
         strict: true,
         format: ['pretty'],
-        require: ['../../stepdefinitions/register.ts'],
+        require: ['../../stepdefinitions/*.ts'],
     }
 };
